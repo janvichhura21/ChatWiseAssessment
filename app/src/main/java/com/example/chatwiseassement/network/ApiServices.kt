f3fe14a2ef746c6da5eeaa33d8f09d018ebd7e67
dell <jchhura21@gmail.com>
2023-05-31 23:24:01 +0530

    done1

--- app/src/main/java/com/example/chatwiseassement/network/ApiServices.kt
+++ app/src/main/java/com/example/chatwiseassement/network/ApiServices.kt
@@ -4,6 +4,8 @@ import com.example.chatwiseassement.model.User
 import retrofit2.Call
 import retrofit2.http.GET
 
+
+
 const val BASE_URL="https://jsonplaceholder.typicode.com/"
 interface ApiServices {
 
